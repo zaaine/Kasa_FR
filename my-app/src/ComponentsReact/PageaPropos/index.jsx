5e613eb74aefc64f44478715dd52344abc3c61c3
zaaine <zaaine@live.fr>
2024-03-15 14:59:56 +0100

    Réalisation de la page A propos et du styles

--- my-app/src/ComponentsReact/PageaPropos/index.jsx
+++ my-app/src/ComponentsReact/PageaPropos/index.jsx
@@ -1,9 +1,27 @@
+// composant React
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+// styles -- images
+import Paysage from '../../Assets/images/Paysage.png'
+import Style_Apropos from '../../Styles/5.Pages/Apropos.scss/Style_Apropos.scss'
+
+
+
 function Apropos(){
     return (
-        <p>Je suis la page A Propos</p>
+        <div className="contenair-apropos" > 
+        <div className="banner-apropos">  
+        <img className="img-apropos" src={Paysage} alt="photo paysage" />
+        </div>
+
+        <div className="rank">  <p>Fiabilité </p> <p className="icones-apropos"> ^ </p> </div>
+        <div className="rank"> <p>Respect </p> <p className="icones-apropos"> ^ </p></div>
+        <div className="rank"> <p>Service </p> <p className="icones-apropos"> ^ </p></div>
+        <div className="rank"> <p>Sécurité </p> <p className="icones-apropos"> ^ </p></div>
+
+        </div>
     )
 }
 
