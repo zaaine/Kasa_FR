a5adc4d8a8c20289fe70adafc54ae8b4e7af6a12
zaaine <zaaine@live.fr>
2024-03-29 21:31:33 +0100

    mise en place du toggle bouton pour affichage description. Correction du Style. Rajout de liste keys pour tags et équipements.

--- my-app/src/ComponentsReact/AnnonceLogement/index.jsx
+++ my-app/src/ComponentsReact/AnnonceLogement/index.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
-// import ReactStars from "react-rating-stars-component";
+
 import { Rating } from 'react-simple-star-rating';
 
 // Files
@@ -12,6 +12,7 @@ import arrow_back from '../../Assets/images/Arrows/arrow_back_ios-24px 2.png'
 import Home from "../Home";
 
 
+
 function AnnonceLogement (){
 
 const [value, setValue] = useState("")
@@ -19,6 +20,12 @@ const params = useParams()
 const  annonceID = params.id
 
 // console.log(params.id)
+
+    const [toggle, setToggle] = useState(false)
+
+    const toggleFunction = () => {
+        setToggle(!toggle)
+    }
 
 useEffect(() => { 
     annonces.map ((annonce, index) => {
@@ -36,6 +43,8 @@ useEffect(() => {
     
    
     return (
+
+// Partie carroussel
         <div className="annonce-contenaire"> 
         
         <div className="annonce-imgs">
@@ -43,6 +52,7 @@ useEffect(() => {
         </div>
 
         <div>
+{/* Partie titre à mettre dans un componement  */}
         <div className="contenaire_titre">
             <h1 className="annonce-title">{value.title} </h1>
             <div className="contenaire_identite">
@@ -54,9 +64,10 @@ useEffect(() => {
         </div>
         
         <section className="contenaire_comments">
-        <div className="contenaire_tags"> 
-        <div className="annonce-tags"> {value.tags[0]} </div>
-        <div className="annonce-tags"> {value.tags[1]} </div>
+        <div className="contenaire_tags"> {value.tags.map((tag,i) => (
+            <p key={i} className="annonce-tags">{tag}</p>
+        ))} 
+        
         </div>
         
         <div className="stars-wrapper">
@@ -67,32 +78,39 @@ useEffect(() => {
                  fillClassName='stars-notation'
                  fillColor="#ff6060"
                  value={value.rating}
-                 size={'35px'}   
+                 size={'30px'}   
                  />
         </div>
         </section>
-
+{/* Partie description à mettre dans un componement */}
         <section className="box-description-equipments"> 
         <div >
         <div className="contenaire_description"> 
         <div > Description </div>
-        <img className="arrow-back" src={arrow_back} alt="ouvrir"/>
+        <img onClick={toggleFunction} className={toggle ? "arrow" : "arrow-back"} src={arrow_back} alt="ouvrir"/>
         </div>
-        <div className="annonce-description" >{value.description}</div>
+        <div className={toggle ? "annonce-description" : "annonce-clear"}>{value.description}</div>
         </div>
 
         <div>
         <div className="contenaire_equipments">
         <div >Équipements</div>
-        <img className="arrow-back" src={arrow_back} alt="ouvrir"/>
+        <img onClick={toggleFunction} className={toggle ? "arrow" : "arrow-back"} src={arrow_back} alt="ouvrir"/>
         </div>
         <div>
-        <div className="annonce-equipments">{value.equipments} </div>
+        <div className={toggle ? "annonce-equipments" : "annonce-clear"}>{value.equipments.map((equipment,i)=>(
+            <ul key={i}>
+                <li>{equipment}</li>
+            </ul>
+        ))} </div>
         </div>
         </div>
         </section>
         
+        {/* <App/> */}
+
         </div>
+
     )
 }
 
