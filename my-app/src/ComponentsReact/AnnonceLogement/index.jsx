efafeba790f9ff306ab48da145e164d9118dcb60
zaaine <zaaine@live.fr>
2024-03-31 04:14:47 +0200

    indentations

--- my-app/src/ComponentsReact/AnnonceLogement/index.jsx
+++ my-app/src/ComponentsReact/AnnonceLogement/index.jsx
@@ -1,28 +1,25 @@
 // React
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from 'react'
 // import { Link } from "react-router-dom"
-import { useParams } from "react-router-dom"
-import { Rating } from "react-simple-star-rating"
+import { useParams } from 'react-router-dom'
+import { Rating } from 'react-simple-star-rating'
 
 // Files
-import annonces from "../../Assets/AnnoncesLogement/annonces.json"
-import Page_Annonce from "../../Styles/5.Pages/Annonce.scss/Page_Annonce.scss"
-import arrow_back from "../../Assets/images/Arrows/arrow_back_ios-24px 2.png"
-import Home from "../Home"
+import annonces from '../../Assets/AnnoncesLogement/annonces.json'
+import '../../Styles/5.Pages/Annonce.scss/Page_Annonce.scss'
+import arrow_back from '../../Assets/images/Arrows/arrow_back_ios-24px 2.png'
 
 function AnnonceLogement() {
-  const [value, setValue] = useState("")
+  const [value, setValue] = useState('')
   const params = useParams()
   const annonceID = params.id
 
   const [toggle, setToggle] = useState(false)
-
   const toggleFunction = () => {
     setToggle(!toggle)
   }
 
   const [view, setView] = useState(false)
-
   const toggleFunctionBis = () => {
     setView(!view)
   }
@@ -35,7 +32,7 @@ function AnnonceLogement() {
     })
   }, [])
 
-  if (value === "") {
+  if (value === '') {
     return
   }
 
@@ -46,8 +43,8 @@ function AnnonceLogement() {
         <img className="annonce-img" src={value.cover} alt="" />
       </div>
 
+      {/* Partie 2 */}
       <div>
-        {/* Partie 2 */}
         <div className="contenaire_titre">
           <h1 className="annonce-title">{value.title} </h1>
           <div className="contenaire_identite">
@@ -64,7 +61,7 @@ function AnnonceLogement() {
 
       <section className="contenaire_comments">
         <div className="contenaire_tags">
-          {" "}
+          {' '}
           {value.tags.map((tag, i) => (
             <p key={i} className="annonce-tags">
               {tag}
@@ -80,10 +77,11 @@ function AnnonceLogement() {
             fillClassName="stars-notation"
             fillColor="#ff6060"
             value={value.rating}
-            size={"30px"}
+            size={'30px'}
           />
         </div>
       </section>
+
       {/* Partie 3 */}
       <section className="box-description-equipments">
         <div>
@@ -91,12 +89,12 @@ function AnnonceLogement() {
             <div> Description </div>
             <img
               onClick={toggleFunction}
-              className={toggle ? "arrow" : "arrow-back"}
+              className={toggle ? 'arrow' : 'arrow-back'}
               src={arrow_back}
               alt="ouvrir"
             />
           </div>
-          <div className={toggle ? "annonce-description" : "annonce-clear"}>
+          <div className={toggle ? 'annonce-description' : 'annonce-clear'}>
             {value.description}
           </div>
         </div>
@@ -106,18 +104,18 @@ function AnnonceLogement() {
             <div>Équipements</div>
             <img
               onClick={toggleFunctionBis}
-              className={view ? "arrow" : "arrow-back"}
+              className={view ? 'arrow' : 'arrow-back'}
               src={arrow_back}
               alt="ouvrir"
             />
           </div>
           <div>
-            <div className={view ? "annonce-equipments" : "annonce-clear"}>
+            <div className={view ? 'annonce-equipments' : 'annonce-clear'}>
               {value.equipments.map((equipment, i) => (
                 <div key={i}>
                   <p className="equipement">{equipment}</p>
                 </div>
-              ))}{" "}
+              ))}{' '}
             </div>
           </div>
         </div>
